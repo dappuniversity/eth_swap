7fc089f7208b00918c746285bdbe2c4886d0be0e
gwmccubbin <gwmccubbin@gmail.com>
2020-03-05 12:23:59 -0600

    1. Smoke Test

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  // Transfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
+};
