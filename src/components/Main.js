5d152f463832072512e2c41cc0fc717b1110a04e
Myk <89474773+mykcryptodev@users.noreply.github.com>
2021-09-02 22:09:52 -0400

    shadow for card

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,7 +55,7 @@ class Main extends Component {
           </button>
         </div>
 
-        <div className="card mb-4 rounded">
+        <div className="card mb-4 rounded shadow">
 
           <div className="card-body">
 
