8779209ba4a96f3c121868380ff9c2b04d3624ca
Myk <89474773+mykcryptodev@users.noreply.github.com>
2021-09-11 16:30:40 -0400

    compile for abis

--- truffle-config.js
+++ truffle-config.js
@@ -13,6 +13,7 @@ module.exports = {
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: '0.6.12',
       optimizer: {
         enabled: true,
         runs: 200
